ab8d56f4512c7ce430cf4b4e2a178fbd12aadc3e
Suren Atoyan <surenatoyan93@gmail.com>
2018-08-28 11:17:38 +0400

    Client ::: Add config file

---
+++ client/src/config/index.js
@@ -0,0 +1,15 @@
+const themes = {
+  editorMod: {
+    backgrountColor: '#202124',
+  },
+};
+
+const helperTexts = { };
+
+const messages = { };
+
+export {
+  themes,
+  helperTexts,
+  messages,
+};
