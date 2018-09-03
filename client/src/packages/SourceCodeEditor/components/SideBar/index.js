af6bd519174ee9da0fe510e07a1eb528cc28b1db
Suren Atoyan <surenatoyan93@gmail.com>
2018-09-03 12:19:55 +0400

    Client ::: ContextMenu ::: Move logic of context menu to sidebar

--- client/src/packages/SourceCodeEditor/components/SideBar/index.js
+++ client/src/packages/SourceCodeEditor/components/SideBar/index.js
@@ -1,9 +1,94 @@
-import React from 'react';
+import React, { PureComponent, Fragment } from 'react';
 
 import TreeView from './TreeView';
+import ContextMenu from './ContextMenu';
 
 const sideBarStyles = { width: '100%', height: '100%' };
 
-const SideBar = props => <div style={sideBarStyles}><TreeView {...props} /></div>;
+class SideBar extends PureComponent {
+
+  state = {
+    anchorElOfOpenedContextMenu: null,
+    contextMenuType: 'file',
+  };
+
+  // NOTE ::: It's just for demo and should be removed
+  commonItems = [
+    {
+      name: 'Rename',
+      action: _ => console.log('Log ::: You want to rename it') &
+        this.handleCloseContextMenu(),
+    },
+    {
+      name: 'Remove',
+      action: _ => console.log('Log ::: You want to remove it') &
+        this.handleCloseContextMenu(),
+    },
+    {
+      name: 'Cut',
+      action: _ => console.log('Log ::: You want to cut it') &
+        this.handleCloseContextMenu(),
+    },
+    {
+      name: 'Copy',
+      action: _ => console.log('Log ::: You want to copy it') &
+        this.handleCloseContextMenu(),
+    },
+    {
+      name: 'Copy Name',
+      action: _ => console.log('Log ::: You want to copy name of it') &
+        this.handleCloseContextMenu(),
+    },
+    {
+      name: 'Copy Path',
+      action: _ => console.log('Log ::: You want to copy path of it') &
+        this.handleCloseContextMenu(),
+    },
+  ];
+
+  contextMenuItems = {
+    'file': [...this.commonItems, ...[{
+      name: 'Copy as Text',
+      action: _ => console.log('Log ::: You want to copy as text it') &
+        this.handleCloseContextMenu(),
+    }]],
+
+    'folder': [...this.commonItems],
+  };
+
+  handleContextMenuOpen = (type, target) => this.setState({
+    anchorElOfOpenedContextMenu: target,
+    contextMenuType: type,
+  });
+
+  handleFolderContextMenuOpen = ({ currentTarget }) =>
+    this.handleContextMenuOpen('folder', currentTarget);
+
+  handleFileContextMenuOpen = ({ currentTarget }) =>
+    this.handleContextMenuOpen('file', currentTarget);
+
+  handleCloseContextMenu = _ => this.setState({ anchorElOfOpenedContextMenu: null });
+
+  render() {
+    const { anchorElOfOpenedContextMenu, contextMenuType } = this.state;
+
+    return (
+      <Fragment>
+        <div style={sideBarStyles}>
+          <TreeView 
+            {...this.props}
+            handleFolderContextMenuOpen={this.handleFolderContextMenuOpen}
+            handleFileContextMenuOpen={this.handleFileContextMenuOpen}
+          />
+        </div>
+        <ContextMenu
+          anchorEl={anchorElOfOpenedContextMenu}
+          menuItems={this.contextMenuItems[contextMenuType]}
+          handleClose={this.handleCloseContextMenu}
+        />
+      </Fragment>
+    );
+  }
+}
 
 export default SideBar;
--- client/src/packages/SourceCodeEditor/index.js
+++ client/src/packages/SourceCodeEditor/index.js
@@ -5,7 +5,7 @@ import Splitter from 'm-react-splitters';
 // Components
 import Editor from './components/Editor';
 import SideBar from './components/SideBar';
-import ContextMenu from './components/SideBar/ContextMenu';
+import StatusBar from './components/StatusBar';
 
 // Config
 import { sampleFileStructure, mapExtToLang } from 'config';
@@ -18,53 +18,7 @@ class SourceCodeEditor extends PureComponent {
 
   state = {
     updateLine: 0,
-    anchorElOfOpenedContextMenu: null,
-    contextMenuType: 'file',
     openFilePath: null,
-  };
-
-  // NOTE ::: It's just for demo and should be removed
-  commonItems = [
-    {
-      name: 'Rename',
-      action: _ => console.log('Log ::: You want to rename it') &
-        this.handleCloseContextMenu(),
-    },
-    {
-      name: 'Remove',
-      action: _ => console.log('Log ::: You want to remove it') &
-        this.handleCloseContextMenu(),
-    },
-    {
-      name: 'Cut',
-      action: _ => console.log('Log ::: You want to cut it') &
-        this.handleCloseContextMenu(),
-    },
-    {
-      name: 'Copy',
-      action: _ => console.log('Log ::: You want to copy it') &
-        this.handleCloseContextMenu(),
-    },
-    {
-      name: 'Copy Name',
-      action: _ => console.log('Log ::: You want to copy name of it') &
-        this.handleCloseContextMenu(),
-    },
-    {
-      name: 'Copy Path',
-      action: _ => console.log('Log ::: You want to copy path of it') &
-        this.handleCloseContextMenu(),
-    },
-  ];
-
-  contextMenuItems = {
-    'file': [...this.commonItems, ...[{
-      name: 'Copy as Text',
-      action: _ => console.log('Log ::: You want to copy as text it') &
-        this.handleCloseContextMenu(),
-    }]],
-
-    'folder': [...this.commonItems],
   };
 
   handleFileOpen = path =>
@@ -75,24 +29,9 @@ class SourceCodeEditor extends PureComponent {
 
   preventDefaultContextMenuBehavior = ev => ev.preventDefault();
 
-  handleContextMenuOpen = (type, target) => this.setState({
-    anchorElOfOpenedContextMenu: target,
-    contextMenuType: type,
-  });
-
-  handleFolderContextMenuOpen = ({ currentTarget }) =>
-    this.handleContextMenuOpen('folder', currentTarget);
-
-  handleFileContextMenuOpen = ({ currentTarget }) =>
-    this.handleContextMenuOpen('file', currentTarget);
-
-  handleCloseContextMenu = _ => this.setState({ anchorElOfOpenedContextMenu: null });
-
   render() {
     const {
       openFilePath,
-      anchorElOfOpenedContextMenu,
-      contextMenuType,
     } = this.state;
 
     const language = openFilePath
@@ -116,8 +55,6 @@ class SourceCodeEditor extends PureComponent {
           >
             <SideBar
               handleFileOpen={this.handleFileOpen}
-              handleFolderContextMenuOpen={this.handleFolderContextMenuOpen}
-              handleFileContextMenuOpen={this.handleFileContextMenuOpen}
               data={sampleFileStructure}
               openFilePath={openFilePath}
             />
@@ -128,12 +65,8 @@ class SourceCodeEditor extends PureComponent {
               language={language}
             />
           </Splitter>
+          <StatusBar />
         </div>
-        <ContextMenu
-          anchorEl={anchorElOfOpenedContextMenu}
-          menuItems={this.contextMenuItems[contextMenuType]}
-          handleClose={this.handleCloseContextMenu}
-        />
       </Fragment>
     );
   }
